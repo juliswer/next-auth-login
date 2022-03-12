22fbf827cf12867ac43690e2516cbd9469464b6d
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 10:24:24 -0300

    adding the instagramprovider from next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,16 @@
-import NextAuth from 'next-auth';
-import GitHubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
 import InstagramProvider from "next-auth/providers/instagram";
 
 export default NextAuth({
-    providers: [
-        GitHubProvider({
-          clientId: process.env.GITHUB_CLIENT_ID,
-          clientSecret: process.env.GITHUB_CLIENT_SECRET
-        }),
-        InstagramProvider({
-          clientId: process.env.INSTAGRAM_CLIENT_ID,
-          clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
-        })
-    ]
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+    })
+  ],
 })
